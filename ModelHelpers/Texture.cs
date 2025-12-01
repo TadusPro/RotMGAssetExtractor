8f6e7f0a4cb903877a6cf3c64eb1792ddc4a29e3
Tadus <tadus@tadus.se>
2025-12-01 18:50:43 +0100

    Add XmlIgnore properties for offset fields in Texture
    Introduced xOffsetSpecified and yOffsetSpecified properties with XmlIgnore attributes to control XML serialization of offset fields in the Texture class. This helps ensure offsets are only serialized when non-zero.

--- ModelHelpers/Texture.cs
+++ ModelHelpers/Texture.cs
@@ -1,4 +1,6 @@
-﻿namespace RotMGAssetExtractor.ModelHelpers
+﻿using System.Xml.Serialization;
+
+namespace RotMGAssetExtractor.ModelHelpers
 {
     public class Texture : ITexture
     {
@@ -6,5 +8,10 @@
         public int Index { get; set; }
         public int xOffset { get; set; }
         public int yOffset { get; set; }
+        [XmlIgnore]
+        public bool xOffsetSpecified => xOffset != 0;
+
+        [XmlIgnore]
+        public bool yOffsetSpecified => yOffset != 0;
     }
 }
